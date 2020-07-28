baa201e035db1a04983cfcfcf11468f719d4132d
snare87 <rhdmf7734@gmail.com>
2020-07-28 10:54:30 +0900

    Login_Picture

---
+++ app/src/main/java/com/example/rhdmf/LoginActivity.java
@@ -0,0 +1,144 @@
+package com.example.rhdmf;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.kakao.auth.ApiErrorCode;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.usermgmt.response.model.Profile;
+import com.kakao.usermgmt.response.model.UserAccount;
+import com.kakao.util.OptionalBoolean;
+import com.kakao.util.exception.KakaoException;
+
+import static com.example.rhdmf.GlobalApplication.getGlobalApplicationContext;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private SessionCallback sessionCallback;
+    private Button btn_custom_login_out;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btn_custom_login_out = (Button) findViewById(R.id.btn_custom_login_out);
+
+        sessionCallback = new SessionCallback(); //SessionCallback 초기화
+        Session.getCurrentSession().addCallback(sessionCallback); //현재 세션에 콜백 붙임
+        //Session.getCurrentSession().checkAndImplicitOpen(); //자동 로그인
+
+
+        btn_custom_login_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserManagement.getInstance()
+                        .requestLogout(new LogoutResponseCallback() {
+                            @Override
+                            public void onCompleteLogout() {
+                                Toast.makeText(getGlobalApplicationContext(),"로그아웃 되었습니다. ",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) { //카카오 로그인 액티비티에서 넘어온 경우일 때 실행
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Session.getCurrentSession().removeCallback(sessionCallback);
+        //현재 액티비티 제거 시 콜백도 같이 제거
+    }
+
+    private class SessionCallback implements ISessionCallback {
+        @Override
+        public void onSessionOpened() {
+            UserManagement.getInstance().me(new MeV2ResponseCallback() {
+                @Override
+                public void onFailure(ErrorResult errorResult) {
+                    int result = errorResult.getErrorCode();
+
+                    if(result == ApiErrorCode.CLIENT_ERROR_CODE) {
+                        Toast.makeText(getApplicationContext(), "네트워크 연결이 불안정합니다. 다시 시도해 주세요.", Toast.LENGTH_SHORT).show();
+                        finish();
+                    } else {
+                        Toast.makeText(getApplicationContext(),"로그인 도중 오류가 발생했습니다: "+errorResult.getErrorMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onSessionClosed(ErrorResult errorResult) {
+                    Toast.makeText(getApplicationContext(),"세션이 닫혔습니다. 다시 시도해 주세요: "+errorResult.getErrorMessage(),Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onSuccess(MeV2Response result) {
+                    Log.i("KAKAO_API", "사용자 아이디: " + result.getId());
+
+                    UserAccount kakaoAccount = result.getKakaoAccount();
+                    if (kakaoAccount != null) {
+
+                        // 이메일
+                        String email = kakaoAccount.getEmail();
+
+                        if (email != null) {
+                            Log.i("KAKAO_API", "email: " + email);
+
+                        } else if (kakaoAccount.emailNeedsAgreement() == OptionalBoolean.TRUE) {
+                            // 동의 요청 후 이메일 획득 가능
+                            // 단, 선택 동의로 설정되어 있다면 서비스 이용 시나리오 상에서 반드시 필요한 경우에만 요청해야 합니다.
+
+                        } else {
+                            // 이메일 획득 불가
+                        }
+
+                        // 프로필
+                        Profile profile = kakaoAccount.getProfile();
+
+                        if (profile != null) {
+                            Log.d("KAKAO_API", "nickname: " + profile.getNickname());
+                            Log.d("KAKAO_API", "profile image: " + profile.getProfileImageUrl());
+                            Log.d("KAKAO_API", "thumbnail image: " + profile.getThumbnailImageUrl());
+
+                        } else if (kakaoAccount.profileNeedsAgreement() == OptionalBoolean.TRUE) {
+                            // 동의 요청 후 프로필 정보 획득 가능
+
+                        } else {
+                            // 프로필 획득 불가
+                        }
+                    }
+
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.putExtra("nikname", result.getNickname());
+                    intent.putExtra("profile", result.getProfileImagePath());
+
+                    startActivity(intent);
+                    finish();
+                }
+            });
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException e) {
+            Toast.makeText(getApplicationContext(), "로그인 도중 오류가 발생했습니다. 인터넷 연결을 확인해주세요: "+e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/rhdmf/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.rhdmf;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.kakao.auth.AuthType;
+import com.kakao.auth.Session;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+
+public class MainActivity extends AppCompatActivity {
+    String strNickname, strProfile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tvNickname = findViewById(R.id.tvNickname);
+        TextView tvProfile = findViewById(R.id.tvProfile);
+
+        Intent intent = getIntent();
+        strNickname = intent.getStringExtra("nikname");
+        strProfile = intent.getStringExtra("profile");
+
+        tvNickname.setText(strNickname);
+        tvProfile.setText(strProfile);
+    }
+}
+
+
